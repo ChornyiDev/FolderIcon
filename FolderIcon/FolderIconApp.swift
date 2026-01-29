89a47c34815cc71a6a50120b1f923182b8469a03
ChornyiDev <chorniy098@gmail.com>
2026-01-29 14:51:49 +0200

    Initial Commit

---
+++ FolderIcon/FolderIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderIconApp.swift
+//  FolderIcon
+//
+//  Created by Yevhen Chornyi on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FolderIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
